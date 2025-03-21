ef6f1d5962d13c5a299ecb533b6ae4a81c106e94
SHREE <mansikothari46@gmail.com>
2025-03-21 13:26:13 +0530

    Add write json to add students and request Validation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/KothariMansi/students-api/internal/config"
+	"github.com/KothariMansi/students-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -22,9 +23,7 @@ func main() {
 
 	// setup router
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Welcome to students API"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 	fmt.Println("Server setup done")
 
 	// setup server
